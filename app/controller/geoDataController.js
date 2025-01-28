75f881b3b51a57c3158f768720c32de2c7c51115
Cade White <cbwhite2@student.fullsail.edu>
2025-01-27 21:41:39 -0700

    Updating files for 3.6 Unit Testing API

--- app/controller/geoDataController.js
+++ app/controller/geoDataController.js
@@ -44,6 +44,8 @@ const getGeoDataByLocation = async (req, res) => {
       data: {
         weatherData: data,
         geoData,
+        page,
+        limit,
       },
     });
   } catch (error) {
